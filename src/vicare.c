3fc840383472f3d3934596e407044514502b0f03
Marco Maggi <marco.maggi-ipsu@poste.it>
2012-01-21 09:55:21 +0100

    added some C functions

--- src/vicare.c
+++ src/vicare.c
@@ -34,8 +34,68 @@
 #include <vicare.h>
 #include <expat.h>
 
+#define EX_PARSER(PARSER)	IK_POINTER_DATA_VOIDP(PARSER)
+#define EX_CALLBACK(CALLBACK)	IK_POINTER_DATA_VOIDP(CALLBACK)
+
 
+/** --------------------------------------------------------------------
+ ** Callbacks.
+ ** ----------------------------------------------------------------- */
 
+ikptr
+ik_expat_set_element_decl_handler (ikptr s_parser, ikptr s_callback)
+{
+  XML_SetElementDeclHandler(EX_PARSER(s_parser), EX_CALLBACK(s_callback));
+  return void_object;
+}
+ikptr
+ik_expat_set_attlist_decl_handler (ikptr s_parser, ikptr s_callback)
+{
+  XML_SetAttlistDeclHandler(EX_PARSER(s_parser), EX_CALLBACK(s_callback));
+  return void_object;
+}
+ikptr
+ik_expat_set_xml_decl_handler (ikptr s_parser, ikptr s_callback)
+{
+  XML_SetXmlDeclHandler(EX_PARSER(s_parser), EX_CALLBACK(s_callback));
+  return void_object;
+}
 
+
+/** --------------------------------------------------------------------
+ ** Parser.
+ ** ----------------------------------------------------------------- */
+
+ikptr
+ik_expat_parser_create (ikptr s_encoding, ikpcb * pcb)
+{
+  const XML_Char *	encoding = IK_BYTEVECTOR_DATA_VOIDP(s_encoding);
+  XML_Parser		parser;
+  parser = XML_ParserCreate(encoding);
+  return ika_pointer_alloc(pcb, (ik_ulong)parser);
+}
+ikptr
+ik_expat_parser_create_ns (ikptr s_encoding, ikptr s_namespace_separator, ikpcb * pcb)
+{
+  const XML_Char *	encoding  = IK_BYTEVECTOR_DATA_VOIDP(s_encoding);
+  XML_Char		separator = (XML_Char)ik_integer_to_int(s_namespace_separator);
+  XML_Parser		parser;
+  parser = XML_ParserCreateNS(encoding, separator);
+  return ika_pointer_alloc(pcb, (ik_ulong)parser);
+}
+#if 0 /* Excluded because, so far, I have  no idea about how to create a
+	 meaningful  memory  functions  suite.   (Marco Maggi;  Jan  21,
+	 2012) */
+ikptr
+ik_expat_parser_create_mm (ikptr s_encoding, ikptr s_namespace_separator, ikpcb * pcb)
+{
+  const XML_Char *	encoding = IK_BYTEVECTOR_DATA_VOIDP(s_encoding);
+  XML_Char		separator = (XML_Char)ik_integer_to_int(s_namespace_separator);
+  const XML_Memory_Handling_Suite * memsuite = NULL;
+  XML_Parser		parser;
+  parser = XML_ParserCreate_MM(encoding, memsuite, separator);
+  return ika_pointer_alloc(pcb, (ik_ulong)parser);
+}
+#endif
 
 /* end of file */
